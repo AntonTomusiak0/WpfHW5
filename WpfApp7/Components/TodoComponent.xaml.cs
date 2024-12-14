7af8dcbd1a3be0ed657dc77bd59fcdcef92d5c68
Tomus_kd35 <Tomus_kd35@student.itstep.org>
2024-12-14 21:01:14 +0200

    Add project files.

---
+++ WpfApp7/Components/TodoComponent.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp7.Data;
+
+namespace WpfApp7.Components
+{
+    /// <summary>
+    /// Interaction logic for TodoComponent.xaml
+    /// </summary>
+    public partial class TodoComponent : UserControl
+    {
+        private Todo todo;
+        public TodoComponent(Todo todo)
+        {
+            InitializeComponent();
+            this.todo = todo;
+        }
+
+        public void OnCompleteClick(object sender, RoutedEventArgs e)
+        {
+            TodoList.Complete(todo.Id);
+            TitleLabel.Foreground = Brushes.Green;
+            CompleteButton.IsEnabled = false;
+        }
+        public void OnDeleteClick(object sender, RoutedEventArgs e)
+        {
+            TodoList.Delete(todo.Id);
+            StackPanel parent = Parent as StackPanel;
+            parent.Children.Remove(this);
+        }
+
+        private void TitleLabel_Loaded(object sender, RoutedEventArgs e)
+        {
+            TitleLabel.Content = todo.Title;
+        }
+    }
+}
---
+++ WpfApp7/Data/TodoList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp7.Data
+{
+    public static class TodoList
+    {
+        private static List<Todo> todos = [];
+
+        public static Todo Add(string title)
+        {
+            var newTodo = new Todo
+            {
+                Title = title,
+                IsCompleted = true,
+                Id = Guid.NewGuid(),
+            };
+            todos.Add(newTodo);
+            return newTodo;
+        }
+        public static void Delete(Guid id)
+        {
+            todos.RemoveAll(t => t.Id == id);
+        }
+        public static void Complete(Guid id)
+        {
+            Todo todo = todos.First(t =>  t.Id == id);
+            todo.IsCompleted = true;
+        }
+        public static void Clear()
+        {
+            todos.Clear();
+        }
+    }
+}
